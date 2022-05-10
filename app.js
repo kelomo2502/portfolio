08010801735af0e293a308ae2084a18e8ad95bfe
HanningtoneM <hanningtonepompyy@gmail.com>
2022-05-10 16:52:47 +0300

    Add eventlistener to display none

--- app.js
+++ app.js
@@ -16,3 +16,9 @@ function close() {
 
 openMenu.addEventListener('click', show); 
 closeMenu.addEventListener('click', close);  
+
+document.querySelectorAll('.menu').forEach((menu) => { 
+  menu.addEventListener('click', () => { 
+    mobileMenu.style.display = 'none'; 
+  }); 
+});
