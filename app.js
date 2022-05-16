8b8682aa0f47a9a452ebc3442592aa254c0801cd
Oyewunmi Gbenga <kelvinoye@gmail.com>
2022-05-16 14:45:34 +0100

    Add the show function

---
+++ app.js
@@ -0,0 +1,8 @@
+const openMenu = document.querySelector('.open-menu');
+const mobileMenu = document.querySelector('.nav-links');
+const closeMenu = document.querySelector('.close-menu');
+const show = () => {
+  mobileMenu.style.display = 'flex';
+  mobileMenu.style.top = '0';
+  closeMenu.style.display = 'block';
+};
