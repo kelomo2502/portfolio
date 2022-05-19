7c91248744000127ad0f909390d756eb2fec1d77
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-05-19 23:48:25 +0100

    Add fullName variable retrieval to getfromlocal storage function

--- app.js
+++ app.js
@@ -107,36 +107,6 @@ form.addEventListener('submit', (e) => {
   }
 });
 // LOCAL STORAGE
-// let formInfo = {};
-// function addToLocalStorage() {
-//   formInfo = {
-//     fullName: form.name.value,
-//     email: form.email.value,
-//     message: form.message.value,
-//   };
-
-//   localStorage.setItem('FORM_DATA', JSON.stringify(formInfo));
-// }
-
-// function getFromLocalStorage() {
-//   let data = localStorage.getItem('FORM_DATA');
-
-//   if (data) {
-//     data = JSON.parse(data);
-//     form.name.value = data.name;
-//     form.email.value = data.email;
-//     form.message.value = data.message;
-//   }
-// }
-
-// window.onload = () => {
-//   form.name.addEventListener('input', addToLocalStorage);
-//   form.email.addEventListener('input', addToLocalStorage);
-//   form.message.addEventListener('input', addToLocalStorage);
-
-//   getFromLocalStorage();
-// };
-
 let formInfo = {};
 function addToLocalStorage() {
   formInfo = {
@@ -152,7 +122,7 @@ function getFromLocalStorage() {
 
   if (data) {
     data = JSON.parse(data);
-    form.name.value = data.name;
+    form.name.value = data.fullName;
     form.email.value = data.email;
     form.message.value = data.message;
   };
